8201689e57213fd0986d493120cc742e282bf3f2
elimumford <elimumford@gmail.com>
2013-07-11 17:01:13 -0600

    cleaned up current index.js files

--- index.js
+++ index.js
@@ -2,21 +2,4 @@ var lib = require('./lib')
 , extend = require('extend')
 ;
 
-function isConfigured() {
-	return lib.isConfigured;
-}
-
-function $init($config) {
-	if (typeof $config !== 'undefined' && !isConfigured()) {
-		lib($config);
-	}
-}
-
-extend($init, lib);
-
-Object.defineProperty($init, 'isConfigured', {
-	get: isConfigured,
-	enumerable: true
-});
-
-module.exports = $init;
+module.exports = lib;
